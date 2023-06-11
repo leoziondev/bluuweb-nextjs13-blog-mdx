cb2d988a92a8fcd8d716316db9d85926098f3684
leoziondev <leozionsuporte@gmail.com>
2023-06-11 18:35:02 -0300

    list all posts

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { allPosts, Post } from 'contentlayer/generated'
+
+const posts: Post[] = allPosts
+
+const Posts = () => {
+  return (
+    <div>
+      <h1 className="text-center my-4 text-3xl">Posts</h1>
+      <div className="grid gap-4">
+        {posts.map((post) => (
+          <article key={post.id}>
+            <h2 className="text-2xl">{post.title}</h2>
+            <time className="text-xs">
+              {new Date(post.date).toLocaleDateString('pt-BR', {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+              })}
+            </time>
+          </article>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Posts
