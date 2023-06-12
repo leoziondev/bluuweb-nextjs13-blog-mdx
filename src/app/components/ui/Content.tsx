adb1af28acb5b28abd1141b9c615011c80105866
leoziondev <leozionsuporte@gmail.com>
2023-06-11 23:56:08 -0300

    add custom css content component

--- src/app/components/ui/Content.tsx
+++ src/app/components/ui/Content.tsx
@@ -1,5 +1,10 @@
-const Content = ({ children }: { children: React.ReactNode }) => {
-  return <div className="bg-white rounded-md p-8 my-8">{children}</div>
+interface Props {
+  children: React.ReactNode
+  css?: string
+}
+
+const Content = ({ children, css }: Props) => {
+  return <div className={`bg-white rounded-md p-8 my-8 ${css}`}>{children}</div>
 }
 
 export default Content
