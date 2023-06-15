e2089368aac0b246626232694558837c809807fd
leoziondev <leozionsuporte@gmail.com>
2023-06-15 10:21:14 -0300

    reusable pagination component in page post

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,6 +1,6 @@
 import PostList from '../components/post/PostList'
+import PostPagination from '../components/post/PostPagination'
 import Container from '../components/ui/Container'
-import Content from '../components/ui/Content'
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -8,17 +8,26 @@ import { allPosts, Post } from 'contentlayer/generated'
 
 const posts: Post[] = allPosts.sort((a, b) => b.date.localeCompare(a.date))
 
+const totalPosts = posts.length
+const postsPerPage = 2
+const totalPages = Math.ceil(totalPosts / postsPerPage)
+
 export const metadata = {
   title: 'List all posts',
   description: 'Describe posts - Generate by create next app'
 }
 
 const Posts = () => {
+  const currentPage = 1
+  const offset = (currentPage - 1) * postsPerPage
+  const currentPost = posts.slice(offset, offset + postsPerPage)
+
   return (
     <Container>
       <h1 className="text-center my-4 text-3xl">Posts</h1>
       <div className="grid gap-6 mt-8">
-        <PostList posts={posts} />
+        <PostList posts={currentPost} />
+        <PostPagination totalPages={totalPages} />
       </div>
     </Container>
   )
